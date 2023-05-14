0a7ccc527daf81686de9a33385b1375d39d51d92
Vladimir Panikarovskikh <122010795+vpanikarovskikh@users.noreply.github.com>
2023-05-14 16:48:52 +0500

    Добавлены методы в класс PetRegistry

--- PetRegistry.cs
+++ PetRegistry.cs
@@ -8,5 +8,39 @@ namespace medical_examination
 {
     internal class PetRegistry
     {
+        public static void Select(Dictionary<string, string[]> filterNames, Dictionary<string, string[]> sortNames)
+        {
+            //void убрать, пока что не получилось добавить тип SqlDataReader
+        }
+        
+        public static Array GetPets(Array data)
+        {
+            return data;
+        }
+
+        public static void Select(long id)
+        {
+            //void убрать, пока что не получилось добавить тип SqlDataReader
+        }
+
+        public static void Create(Array data)
+        {
+
+        }
+
+        public static void Insert(Array data)
+        {
+            //void убрать, пока что не получилось добавить тип SqlDataReader
+        }
+
+        public static void Delete(long cardNumber)
+        {
+            //void убрать, пока что не получилось добавить тип SqlDataReader
+        }
+
+        public static void Update(long id, string[] data)
+        {
+            //void убрать, пока что не получилось добавить тип SqlDataReader
+        }
     }
 }
