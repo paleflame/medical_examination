e79ed56b5e3d66f4b2557378896d98f0cb84d432
Danil Vasilenko <vasilenkod.contact@gmail.com>
2023-06-04 15:30:30 +0500

    Class User added

---
+++ User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace medical_examination
+{
+    public class User
+    {
+        public string Name { get; }
+        public string Email { get; }
+        public string Password { get; }
+        public string Role { get; }
+        public Organization Organization { get; }
+       
+        public User(string name, 
+                    string email, 
+                    string password, 
+                    string role, 
+                    Organization organization) 
+        { 
+            Name = name;
+            Email = email;
+            Password = password;
+            Role = role;
+            Organization = organization;
+        }
+    }
+}
