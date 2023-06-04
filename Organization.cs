c3b60da9ee5af5220f6d4609cae6b7364f97f76a
Danil Vasilenko <vasilenkod.contact@gmail.com>
2023-06-04 23:03:52 +0500

    Organization and Permissions

--- Organization.cs
+++ Organization.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,30 @@ namespace medical_examination
 {
     public class Organization
     {
+        public string Name { get; }
+        public int INN { get; }
+        public int KPP { get; }
+        public string Address { get; }
+
+        public string Type { get; }
+        public bool LegalEntity { get; }
+
+        public Organization(string name,
+                            int inn, 
+                            int kpp, 
+                            string address, 
+                            string type, 
+                            bool legalEntity)
+        {
+            Name = name;
+            INN = inn;
+            KPP = kpp;
+            Address = address;
+            Type = type;
+            LegalEntity = legalEntity;
+
+        }
+
+
     }
 }
