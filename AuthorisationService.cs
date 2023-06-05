24672d1a5bc2640e2be6f9196f9e79f0cad00478
Danil Vasilenko <85587508+paleflame@users.noreply.github.com>
2023-06-05 12:52:33 +0500

    Update AuthorisationService.cs

--- AuthorisationService.cs
+++ AuthorisationService.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,27 +13,61 @@ namespace medical_examination
         {
             var dbResponse = DB.GetUserData(login, password);
 
-            if (dbResponse != null)
+
+            if (dbResponse == null)
+            {
+                return null;
+            } else
             {
                 return CreateUser(dbResponse);
             }
 
             // тут вроде лучше поднять ошибку
-            return null;
         }
 
         private static User CreateUser(Dictionary<string, string> userdata)
         {
-            return new User( userdata["name"], 
-                             userdata["role"]
-                             //userdata["organization"]
+            var role = DB.getRoleById((userdata["role"]));
+            var organization = DB.getOrganizationById((userdata["organization"]));
+            Organization org = new Organization(organization["name"], organization["kpp"], organization["inn"], organization["address"], organization["type_id"], organization["legal_entity"]);
+            var user = new User( userdata["name"],
+                             role,
+                             org
                              );
+
+
+            return user;
         }
 
-        /*public static IDefinePermissions GetPM(User user)
+        /public static IDefinePermissions GetPM(User user)
         {
             return PMFactory.Get(user);
-        }*/
+        }/
 
+    }
+    class DataBase
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-RDHJ1N2\SQLEXPRESS;Initial Catalog=test;Integrated Security=True");
+
+        public void openConnection()
+        {
+            if (conn.State == System.Data.ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
+
+        public SqlConnection getConnection()
+        {
+            return conn;
+        }
     }
 }
